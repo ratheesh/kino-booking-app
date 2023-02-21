d07d5a40d29ce9e0793346c07e21e5afeca30cc2
Ratheesh S <ratheeshreddy@gmail.com>
2023-02-21 13:46:27 +0530

    fix(db): fix clash of virtual-column names with model names

--- kino/db.py
+++ kino/db.py
@@ -11,11 +11,12 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    email = db.Column(db.String, unique=True, nullable=False)
     name = db.Column(db.String, nullable=False)
-    username = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
     passwd = db.Column(db.String, nullable=False)
-    created_on = db.Column(db.String, nullable=False)  # set to datetime obj
+    email = db.Column(db.String, unique=True, nullable=False)
+    created_on = db.Column(db.String, nullable=False)
+
     bookings = db.relationship("Booking", backref="user")
 
 
@@ -24,6 +25,7 @@ class Venue(db.Model):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String, nullable=False)
     city = db.Column(db.String, unique=True, nullable=False)
+
     shows = db.relationship("Show", backref="venue")
 
     def __repr__(self) -> str:
@@ -41,16 +43,17 @@ class Show(db.Model):
     __tablename__ = "show"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    lang = db.Column(db.String, nullable=False)
+    language = db.Column(db.String, nullable=False)
     duration = db.Column(db.Integer, nullable=False)
-    certification = db.Column(db.String, nullable=False)
+    rating = db.Column(db.String, nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
-    tags = db.Column(db.String)
+    popularity = db.Column(db.Integer, nullable=False)
+    genre = db.Column(db.String)
     show_date = db.Column(db.String, nullable=False)
     show_time = db.Column(db.String, nullable=False)
     banner_path = db.Column(db.String, nullable=False)
-    venue = db.Column(db.Integer, db.ForeignKey("venue.id"))
+
+    venue_id = db.Column(db.Integer, db.ForeignKey("venue.id"))
     tickets = db.relationship("Ticket", backref="show")
     show_tags = db.relationship("Tag", secondary=tags, backref="tag_tags")
 
@@ -70,9 +73,10 @@ class Tag(db.Model):
 class Ticket(db.Model):
     __tablename__ = "ticket"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    booked_at = db.Column(db.String, nullable=False)
-    show = db.Column(db.Integer, db.ForeignKey("show.id"))
-    booking = db.Column(db.Integer, db.ForeignKey("booking.id"))
+    # booked_at = db.Column(db.String, nullable=False)
+
+    show_id = db.Column(db.Integer, db.ForeignKey("show.id"))
+    booking_id = db.Column(db.Integer, db.ForeignKey("booking.id"))
     seats = db.relationship("Seat", backref="ticket")
 
 
@@ -80,8 +84,9 @@ class Seat(db.Model):
     __tablename__ = "seat"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     row = db.Column(db.String, nullable=False)
-    col = db.Column(db.String, nullable=False)
-    ticket = db.Column(db.Integer, db.ForeignKey("ticket.id"))
+    seat = db.Column(db.String, nullable=False)
+
+    ticket_id = db.Column(db.Integer, db.ForeignKey("ticket.id"))
 
 
 class Booking(db.Model):
@@ -89,9 +94,10 @@ class Booking(db.Model):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     booking_date = db.Column(db.String, nullable=False)
     booking_time = db.Column(db.String, nullable=False)
-    user = db.Column(db.Integer, db.ForeignKey("user.id"))
-    venue = db.Column(db.Integer, db.ForeignKey("venue.id"))
-    show = db.Column(db.Integer, db.ForeignKey("show.id"))
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    venue_id = db.Column(db.Integer, db.ForeignKey("venue.id"))
+    show_id = db.Column(db.Integer, db.ForeignKey("show.id"))
     tickets = db.relationship("Ticket", backref="booking")
 
 
