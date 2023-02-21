8ce9e3fd67a1c256503fc09875720364669a0e43
Ratheesh S <ratheeshreddy@gmail.com>
2023-02-21 18:24:10 +0530

    feat(api): implement basic get and post request for Venue

--- kino/api.py
+++ kino/api.py
@@ -2,7 +2,7 @@ from flask import Blueprint, make_response
 from flask_restful import Resource, fields, marshal_with, reqparse
 from werkzeug.exceptions import HTTPException
 
-from .db import User, db
+from .db import Show, User, Venue, db
 
 # from flask_sqlalchemy import SQLAlchemy
 
@@ -17,11 +17,11 @@ class NotFoundError(HTTPException):
 
 
 user_request_parse = reqparse.RequestParser()
-user_request_parse.add_argument("name")
-user_request_parse.add_argument("username")
-user_request_parse.add_argument("password")
-user_request_parse.add_argument("email")
-user_request_parse.add_argument("created_on")
+user_request_parse.add_argument("name", type=ascii)
+user_request_parse.add_argument("username", type=ascii)
+user_request_parse.add_argument("password", type=ascii)
+user_request_parse.add_argument("email", type=ascii)
+user_request_parse.add_argument("created_on", type=ascii)
 
 user_response_fields = {
     "id": fields.Integer,
@@ -53,17 +53,17 @@ class UserAPI(Resource):
 
     @marshal_with(user_response_fields)
     def post(self):
-        args = user_request_parse.parse_args()
+        args = user_request_parse.parse_args(strict=True)
         name = args.get("name", None)
         username = args.get("username", None)
-        passwd = args.get("password", None)
+        password = args.get("password", None)
         email = args.get("email", None)
         created_on = args.get("created_on", None)
 
         new_user = User(
             name=name,
             username=username,
-            passwd=passwd,
+            password=password,
             email=email,
             created_on=created_on,
         )
@@ -79,15 +79,47 @@ class UserAPI(Resource):
         pass
 
 
+venue_request_parse = reqparse.RequestParser()
+venue_request_parse.add_argument("name", type=ascii)
+venue_request_parse.add_argument("city", type=ascii)
+
+venue_response_fields = {
+    "id": fields.Integer,
+    "name": fields.String,
+    "city": fields.String,
+}
+
+
 class VenueAPI(Resource):
-    def get(self):
-        return {"001": "Bengaluru"}
+    @marshal_with(venue_response_fields)
+    def get(self, venue_id=None):
+        if venue_id is not None:
+            venue_details = db.session.query(
+                Venue).filter(Venue.id == venue_id).first()
 
-    # def get(self, venue_id):
-    #     return {"001": "Bengaluru"}
+            if venue_details:
+                return venue_details
+            else:
+                raise NotFoundError(status_code=404)
+        else:  # send details of all users
+            venues = db.session.query(Venue).all()
+            print(Venue)
+            return venues
 
-    def post(self, venue_id):
-        pass
+    @marshal_with(venue_response_fields)
+    def post(self):
+        args = venue_request_parse.parse_args(strict=True)
+        name = args.get("name", None)
+        city = args.get("city", None)
+
+        new_venue = Venue(
+            name=name,
+            city=city,
+        )
+        db.session.add(new_venue)
+        db.session.commit()
+
+        return new_venue, 201
 
     def put(self, venue_id):
         pass
