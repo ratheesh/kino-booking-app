c110ba79ce16e3df1aa965c2a80dac3db6719b20
Ratheesh S <ratheeshreddy@gmail.com>
2023-02-22 19:43:04 +0530

    db: set the DB model field limits

--- kino/db.py
+++ kino/db.py
@@ -11,11 +11,12 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    username = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
+    username = db.Column(db.String(32), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    name = db.Column(db.String(32), nullable=False)
     created_on = db.Column(db.String, nullable=False)
+    created_on = db.Column(db.String(64), nullable=False)
 
     bookings = db.relationship("Booking", backref="user")
 
@@ -26,8 +27,8 @@ class User(db.Model):
 class Venue(db.Model):
     __tablename__ = "venue"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(32), nullable=False)
+    city = db.Column(db.String(32), nullable=False)
 
     shows = db.relationship("Show", backref="venue")
 
@@ -45,16 +46,16 @@ tags = db.Table(
 class Show(db.Model):
     __tablename__ = "show"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    language = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    language = db.Column(db.String(32), nullable=False)
     duration = db.Column(db.Integer, nullable=False)
     rating = db.Column(db.String, nullable=False)
     price = db.Column(db.Integer, nullable=False)
     popularity = db.Column(db.Integer, nullable=False)
-    genre = db.Column(db.String)
-    show_date = db.Column(db.String, nullable=False)
-    show_time = db.Column(db.String, nullable=False)
-    banner_path = db.Column(db.String, nullable=False)
+    genre = db.Column(db.String(64))
+    show_date = db.Column(db.String(64), nullable=False)
+    show_time = db.Column(db.String(64), nullable=False)
+    banner_path = db.Column(db.String(64), nullable=False)
 
     venue_id = db.Column(db.Integer, db.ForeignKey("venue.id"))
     tickets = db.relationship("Ticket", backref="show")
@@ -67,7 +68,7 @@ class Show(db.Model):
 class Tag(db.Model):
     __tablename__ = "tag"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    name = db.Column(db.String(32), nullable=False)
+    name = db.Column(db.String(16), nullable=False)
 
     def __repr__(self) -> str:
         return self.name
@@ -89,8 +90,8 @@ class Ticket(db.Model):
 class Seat(db.Model):
     __tablename__ = "seat"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    row = db.Column(db.String, nullable=False)
-    seat = db.Column(db.String, nullable=False)
+    row = db.Column(db.String(4), nullable=False)
+    seat = db.Column(db.String(4), nullable=False)
 
     ticket_id = db.Column(db.Integer, db.ForeignKey("ticket.id"))
 
@@ -101,8 +102,8 @@ class Seat(db.Model):
 class Booking(db.Model):
     __tablename__ = "booking"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    booking_date = db.Column(db.String, nullable=False)
-    booking_time = db.Column(db.String, nullable=False)
+    booking_date = db.Column(db.String(64), nullable=False)
+    booking_time = db.Column(db.String(64), nullable=False)
 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     venue_id = db.Column(db.Integer, db.ForeignKey("venue.id"))
