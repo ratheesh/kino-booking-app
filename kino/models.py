5512561a4d131cec1d1ab4838cbe1cd7bd88db82
Ratheesh <ratheeshreddy@gmail.com>
2023-04-09 18:31:43 +0530

    remove unused files

--- kino/models.py
+++
@@ -1,7 +0,0 @@
-from .database import db
-
-class User(db.Model):
-    __tablename__='user'
-    user_id=db.Column(db.Integer, autoIncrement=True, primary_key=True)
-    username=db.Column(db.String, unique=True)
-    email=db.Column(db.String, unique=True)
